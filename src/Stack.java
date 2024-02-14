b9c45ee1049991ec6b1ef85637829852e11ff8b0
Julian Burisic <jburisic@student.tgm.ac.at>
2024-02-14 10:58:38 +0100

    push, pop and peek finished

--- src/Stack.java
+++ src/Stack.java
@@ -1,13 +1,30 @@
 public class Stack<T> {
     private T[] stack = (T[]) new Object[3];
+    private int topElement = stack.length;
 
 
-    public Stack(){
-        stack = (T[]) new Object[3];
-    }
 
     public Stack(int length){
         stack = (T[]) new Object[length];
     }
 
+    public void push(T element){
+        stack[topElement--] = element;
+    }
+
+    public T pop() throws StackFullException, StackEmptyException {
+        if(topElement < 0)throw new StackFullException();
+        if(stack[topElement] == null) throw new StackEmptyException();
+        T x = stack[topElement];
+        stack[topElement--] = null;
+        return x;
+    }
+
+    public T peek() throws StackFullException, StackEmptyException {
+        if(topElement < 0)throw new StackFullException();
+        if(stack[topElement] == null) throw new StackEmptyException();
+        return stack[topElement];
+    }
+
+    public String list
 }
