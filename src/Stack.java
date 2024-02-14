7e96a49fa388f51e46621be82e4e32b1054f79cb
Julian Burisic <jburisic@student.tgm.ac.at>
2024-02-14 10:34:45 +0100

    Finished constructors for the generic class

--- src/Stack.java
+++ src/Stack.java
@@ -1,3 +1,13 @@
-public class Stack {
+public class Stack<T> {
+    private T[] stack = (T[]) new Object[3];
+
+
+    public Stack(){
+        stack = (T[]) new Object[3];
+    }
+
+    public Stack(int length){
+        stack = (T[]) new Object[length];
+    }
 
 }
